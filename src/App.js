9e79a3680f3774899c8eb602e87f4224acf5516f
Mark <wowmarkambro@gmail.com>
2020-05-15 18:15:33 -0500

    started work on MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link, Route } from 'react-router-dom';
+import Form from './components/Form';
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Form />
+    </div>
   );
 };
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Form () {
+
+    return (
+        <div>
+            <p>FORM GOES HERE!</p>
+        </div>
+    );
+}
+
+export default Form;
